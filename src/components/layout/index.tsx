c872e13c6ee143d005ea0aeb482c15376a05c5d8
Robert Garcia <rbert.g.ch@gmail.com>
2023-02-06 23:32:13 -0300

    Add Nav Mobile

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,12 +5,16 @@ import {
   ThemeContextProvider,
 } from '../../context/theme/themecontext'
 import HeaderPage from '../header'
+import NavHeader from '../header/nav'
 
 const LayoutPage = () => {
   const SelectTheme = 'light'
   return (
     <ThemeContextProvider>
       <HeaderPage theme={SelectTheme} />
+      <nav className='mobile'>
+        <NavHeader />
+      </nav>
       <main className={`main-content ${SelectTheme}`}>
         <Outlet />
       </main>
